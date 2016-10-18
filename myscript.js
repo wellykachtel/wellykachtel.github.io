acdf3eeb916182a314ef7e65945d76bd78a2705e
wellykachtel <kwachtel24@gmail.com>
2016-10-18 14:32:45 -0500

    title fixes

--- myscript.js
+++ myscript.js
@@ -1,9 +1,44 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 
-	$('#textOnPic1, #textOnPic2').hide();
-	$('#textOnPic1, #textOnPic2').scroll(function(){
-		$('#textOnPic1').fadeIn();
-		$('#textOnPic2').fadeIn();
+	$("#textOnPic2").hide();
+	var fadeFlag = 1;
+	var animateFlag = 1;
+	
+	$(window).scroll(function(){
+		var topOfTextBox2 = $('#textOnPic2').offset().top;
+		var topOfTextBox1 = $('#textOnPic1').offset().top;
+		var bottomOfWindow = $(window).scrollTop() + $(window).height();
+		
+		if((topOfTextBox2 >= 500) && fadeFlag == 1){
+			$('#textOnPic2').fadeIn("slow");
+			
+			fadeFlag = 0;
+		}
+		else if((bottomOfWindow - topOfTextBox2) < 10 ){
+			$('#textOnPic2').hide();
+		
+			fadeFlag = 1;
+		}
+	
+		var win = $(window).scrollTop();
+		console.log("window: " + win);
+		console.log("top of box: " + topOfTextBox1);
+		
+
+		if( (win > 116) && (animateFlag == 1)){
+			animateFlag = 0;
+			$('#websiteTitle').addClass("scrolledTitle");
+			//$('#websiteTitle').removeClass("websiteTitle");
+		}
+
+		else if(win < 116){
+			
+			animateFlag = 1;
+			$('#websiteTitle').removeClass("scrolledTitle");
+			//$('#websiteTitle').addClass("websiteTitle");
+		}
+		
 	});
+	
 
-});*/
+});
